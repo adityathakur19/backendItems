5c266f32d4e992ec01513c63d4cf681c42862f64
Aditya Thakur <71884179+adityathakur19@users.noreply.github.com>
2024-11-27 15:51:36 +0530

    Update productRoutes.js

--- productRoutes.js
+++ productRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Product = require('../models/Product');
+const Product = require('./Product');
 const { validationResult, body } = require('express-validator');
 
 // Validation middleware
